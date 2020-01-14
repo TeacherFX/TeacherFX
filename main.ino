6911fb71d1f5e879b2d9d8fd82aeefdaf25a4611
TeacherFX <d.gannan@cqu.edu.au>
2020-01-14 15:07:35 +1000

    Updating comments

--- main.ino
+++ main.ino
@@ -3,22 +3,21 @@
 #include <Adafruit_BME280.h>
 Adafruit_BME280 bme;
 
-#define SEALEVELPRESSURE_HPA (1013.25)
+#define SEALEVELPRESSURE_HPA (1013.25) //Setting the pressure of sea level, this can be used to estimate altitude using pressure
 
 unsigned long delayTime;
 
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(115200);                    //Recommend leaving this at 115200 so if there is a boot fail you can see it as ESP32 operates on 115200
   bool status;
 
-  // default settings
-  // (you can also pass in a Wire library object like &Wire2)
-  status = bme.begin(0x76);  
-  if (!status) {
+  status = bme.begin(0x76);                //This returns a 1 if the BME is working, and 0 if not
+  if (!status) {                           //If !status means "If Not Status" where status is a 1 or a 0 as per the previous line.
     Serial.println("Could not find a valid BME280 sensor, check wiring!");
-    while (1);
+    delay(2000);                           //Delay in milliseconds
+    while (1);                             //This creates a loop until the BME is detected
   }
   
   Serial.println("-- Default Test --");
@@ -30,7 +29,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int val = analogRead(36);  // read the input pin
+  int val = analogRead(36);  //This is the signal pin connected to the soil moisture sensor
   Serial.print("Soil Moisture: ");
   Serial.print(val);
   Serial.println();
